571726e88e6d31046687df0d49a15e543865b0bd
Nicolas Guigo <nicolas.guigo@nccgroup.com>
2021-03-08 15:21:16 -0800

    More elegant solution
      - Uses path coverage (instead of code coverage ) strategy
      - Uses miasm's win64 sandbox
      - Requires mass recursion

--- snake_solve.py
+++ snake_solve.py
@@ -1,9 +1,11 @@
-import z3
+import sys, z3
 from miasm.core.utils                      import upck16, pck64
+from miasm.core.locationdb                 import LocationDB
+from miasm.core.interval                   import interval
 from miasm.analysis.dse                    import DSEPathConstraint as DSEPC
-from miasm.expression.expression           import ExprInt, ExprId, ExprMem, ExprAssign
+from miasm.expression.expression           import ExprInt, ExprLoc, ExprMem
 from miasm.jitter.csts                     import PAGE_READ, PAGE_WRITE
-from sandbox_win64                         import Sandbox_Win64
+from miasm.analysis.sandbox                import Sandbox_Win_x86_64
 
 # Memory dump of the QT string when trying '123456789ABCDEF0' in GUI
 qtstring =  b'\x06\x00\x00\x00\x10\x00\x00\x00\x14\x00\x00\x00\x0D\xF0\xAD\xBA' \
@@ -11,92 +13,72 @@ qtstring =  b'\x06\x00\x00\x00\x10\x00\x00\x00\x14\x00\x00\x00\x0D\xF0\xAD\xBA' 
             b'\x35\x00\x36\x00\x37\x00\x38\x00\x39\x00\x41\x00\x42\x00\x43\x00' \
             b'\x44\x00\x45\x00\x46\x00\x30\x00\x00\x00\xAD\xBA\x0D\xF0\xAD\xBA'
 
-unicode_string = lambda ba: ' '.join(f'{upck16(ba[i:i+2]):04x}' for i in range(0, len(ba), 2))
+unicode_string = lambda ba: ' '.join(f'{upck16(ba[i:i+2]):04X}' for i in range(0, len(ba), 2))
 
-class FinishException(Exception):
-  def __init__(self, password):
-    super(FinishException, self).__init__('Found incremental solution')
-    self.password = password
+flag_ptr = 0x20000018
+end_ptr = 0x140004C23
+success_ptr = 0x140004C07
 
-def set_unicode_ranges(address, ranges):
-  mem = ExprMem(ExprInt(address, 64), 16)
-  z3_mem = dse.z3_trans.from_expr(dse.eval_expr(mem))
-  for l, u in ranges:
-    lower_bound = z3.UGT(z3_mem, dse.z3_trans.from_expr(ExprInt(l, 16)))
-    upper_bound = z3.UGT(dse.z3_trans.from_expr(ExprInt(u, 16)), z3_mem)
-    constraint = z3.And(lower_bound, upper_bound)
-    dse.cur_solver.add(constraint)
-
+max_scount = 0x0
 def stop_exec(jitter):
-  dse.cur_solver.check()
-  # If we reached a new block
-  for bbl, model in dse.new_solutions.items():
-    bbl_addr = dse.loc_db.get_location_offset(bbl.loc_key) # Get the bbl offset the DSE hit
-    # Capture the values of the two symbolized bytes
-    current[index] =  model.eval(dse.z3_trans.from_expr(dse.memory_to_expr(0x20000018+index))).as_long()
-    current[index+1] = model.eval(dse.z3_trans.from_expr(dse.memory_to_expr(0x20000018+index+1))).as_long()
-    if bbl_addr == 0x140004C07:
-      ustr = current.decode('UTF-16')
-      print(f'\tHit success bbl at {bbl_addr:08X} with tentative password: "{ustr:s}"')
-      raise FinishException(current)
-    else:
-      print(f'\tHit new bbl at {bbl_addr:08X} with: {unicode_string(current):s}')
-      todo.append(current) # Put it back in the todo list
+  global max_scount
+  success_loc = ExprLoc(dse.loc_db.get_offset_location(success_ptr), 64)
+  for path, model in dse.new_solutions.items():
+    scount = path.count(success_loc)
+    if scount > max_scount:
+      candidate = bytearray(0x20)
+      for i in range(0x20):
+        bb = model.eval(dse.z3_trans.from_expr(dse.memory_to_expr(flag_ptr+i)))
+        candidate[i] = bb.as_long() if type(bb)==z3.z3.BitVecNumRef else current[i]
+      ustr = candidate.decode('UTF-16')
+      print(f'\tHit success bbl {scount} times with: "{ustr[:scount]}"')
+      todo.append(bytes(candidate)) # Put it back in the todo list
+      max_scount = scount
   return False # This ends execution
 
-def forward(jitter):
-  jitter.run_at(jitter.pc)
-
+forward = lambda j: bool(j.run_at(j.pc))
 qt_methods = {'qt5core_??AQString@@QEBA?BVQChar@@H@Z': forward, 'qt5core_?toAscii@QChar@@QEBADXZ': forward,
               'qt5core_?at@QString@@QEBA?BVQChar@@H@Z': forward, 'qt5core_?toLatin1@QChar@@QEBADXZ': forward }
 
 def main():
-  global dse, todo, index, current
+  global dse, todo, current
+  sys.setrecursionlimit(2000) # oof
   # Parse arguments
-  parser = Sandbox_Win64.parser(description="PE sandboxer")
+  parser = Sandbox_Win_x86_64.parser(description="PE sandboxer")
   parser.add_argument("filename", help="PE Filename")
   options = parser.parse_args()
-  options.use_seh = True # So we dont need to reimplement qt crap
-  options.dependencies = True # So we dont need to reimplement qt crap
-  options.align_s = False
-  sb = Sandbox_Win64(options.filename, options, custom_methods=qt_methods)
-  sb.jitter.add_breakpoint(0x140004C23, stop_exec) # End condition
-  # Setup our bogus qt string and a pointer to it
+  options.dependencies = True # So we dont need to reimplement qt
+  sb = Sandbox_Win_x86_64(LocationDB(), options.filename, options, custom_methods=qt_methods)
+  sb.jitter.add_breakpoint(end_ptr, stop_exec) # End condition
+  # Setup the qt string memory and a pointer to it
   sb.jitter.vm.add_memory_page(0x10000018, PAGE_READ|PAGE_WRITE, pck64(0x20000000)) # Hooking in here
-  sb.jitter.vm.add_memory_page(0x20000000, PAGE_READ|PAGE_WRITE, qtstring) # The bogus qstring
+  sb.jitter.vm.add_memory_page(0x20000000, PAGE_READ|PAGE_WRITE, qtstring) # The initial qstring
   sb.jitter.vm.add_memory_page(0x10000020, PAGE_READ|PAGE_WRITE, b'\x00') # The result
   sb.jitter.cpu.R15 = 0x10000000
-  # Stick the symbolic execution on it
-  dse = DSEPC(sb.machine, produce_solution=DSEPC.PRODUCE_SOLUTION_CODE_COV)
+  sb.jitter.cpu.RSP = sb.jitter.stack_base+0x8000
+  # Setup and attach the DSE
+  dse = DSEPC(sb.machine, sb.loc_db, produce_solution=DSEPC.PRODUCE_SOLUTION_PATH_COV)
   sb.jitter.init_run(0x140004B61)
   dse.attach(sb.jitter)
   dse.update_state_from_concrete()
+  dse.symbolize_memory(interval([(flag_ptr, flag_ptr+0x20)]))
+  # Printable unicode only
+  for address in range(flag_ptr, flag_ptr+0x20, 0x2):
+    z3_mem = dse.z3_trans.from_expr(dse.eval_expr(ExprMem(ExprInt(address, 64), 16)))
+    unicode_constraint = z3.And( \
+                            z3.UGE(z3_mem, dse.z3_trans.from_expr(ExprInt(0x0020, 16))), \
+                            z3.ULE(z3_mem, dse.z3_trans.from_expr(ExprInt(0x007E, 16))) \
+                            )
+    dse.cur_solver.add(unicode_constraint )
   snapshot = dse.take_snapshot()
   # Begin run
-  password = bytearray(b'\xFF\xFF') # Just one bogus unicode char to start with
-  todo = [password]
-  index = 0
-  new_run = False
+  todo = [b'\x41\x00'*0x10]
   while todo:
+    dse.restore_snapshot(snapshot)
     current = todo.pop()
-    dse.restore_snapshot(snapshot, keep_known_solutions=not new_run)
-    # Update the password in jitter memory
-    for i, c in enumerate(current):
-      sb.jitter.vm.set_u8(0x20000018+i, int(c))
-    # Symbolize the next unicode char (2 bytes)
-    dse.update_state({ExprMem(ExprInt(0x20000018+index, 64), 8) : dse.memory_to_expr(0x20000018+index), \
-                        ExprMem(ExprInt(0x20000018+index+1, 64), 8) : dse.memory_to_expr(0x20000018+index+1)})
-    set_unicode_ranges(0x20000018+index, {(0x0020, 0x007E)}) # Ensure DSE only finds printable unicode chars solutions
-    try:
-      print(('-'*40 + ' NEWRUN\n' if new_run else '') + f'CONCRETE: {unicode_string(current):s}')
-      sb.jitter.continue_run()
-    except FinishException as fini:
-      password = fini.password+bytearray(b'\xFF\xFF')
-      todo = [password]
-      index+=2
-      new_run = True
-      continue
-    new_run = False
+    sb.jitter.vm.set_mem(flag_ptr, current) # Update the password in jitter memory
+    print('-'*40 + f' CONCRETE: {unicode_string(current)}')
+    sb.jitter.continue_run()
 
 if __name__ == '__main__':
   main()
